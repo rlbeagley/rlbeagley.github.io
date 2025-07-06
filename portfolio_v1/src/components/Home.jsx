0a2c6bc962bb7be4e918ab5398eba37d9b710ab4
rlbeagley <rbeagleyuni@gmail.com>
2025-07-05 20:22:48 -0400

    scrapbook background

--- portfolio_v1/src/components/Home.jsx
+++ portfolio_v1/src/components/Home.jsx
@@ -1,19 +1,23 @@
-import meImage from '../assets/me.jpg'
+import meImage from '../assets/me.jpg';
+import scrapbookBackground from '../assets/homeBackground.png';
 import './Home.css'
 
 function Home() {
     return (
         <div id='sectionContainer'>
-            <div id='textContainer'>
-                <h3>Hi, i'm 
-                    <h2 id="name">
-                        Rebecca Li Beagley,
-                    </h2>
-                </h3>
-                <h3>a 3rd year Computer Science student 
-                    @uOttawa interested in 
-                    software development, devops, and data.</h3>
+            <div id='contentWrapper'>
+                <img id='scrapbookBackground' src={scrapbookBackground} />
+                <div id='textContainer'>
+                    <h3>Hi, i'm 
+                        <h2 id="name">
+                            Rebecca Li Beagley,
+                        </h2>
+                    </h3>
+                    <h3>and I like to</h3>
+                 </div>
             </div>
+            
+            
             <div id= 'photoContainer'>
                 <img id='photoOfMe' src={meImage} 
                     alt="Photo of Rebecca Beagley" />
